c99019736cb2bc37958bb21a07a6d69b63b8e997
Dikpaal <dikpaal@outlook.com>
2023-10-03 12:36:14 -0700

    changes the plane such that each coordinate is printed out as a number

--- src/main/ui/Room.java
+++ src/main/ui/Room.java
@@ -2,21 +2,39 @@ package ui;
 
 import model.Dimension;
 
+import java.util.List;
+
 public class Room {
     Dimension dimension = new Dimension(10, 10);
+    List<List<String>> plane;
 
     public void printRoom() {
-        System.out.print("/ ");
-        System.out.print(".".repeat((dimension.getLength() - 1) * 4));
-        System.out.println(". \\");
 
-        for (int i = 0; i < dimension.getWidth(); i++) {
-            System.out.print("|");
-            System.out.println(" _ |".repeat(dimension.getLength()));
+        int count = 0;
 
+        for (int i = 0; i < dimension.getLength() * dimension.getWidth(); i++) {
+            int number = i + 1;
+
+            if (number < 10) {
+                System.out.print("   " + number + " ");
+            } else if (number < 100) {
+                System.out.print("  " + number + " ");
+            }
+            if (number >= 100) {
+                System.out.print(" " + number + " ");
+            }
+            count++;
+
+            if (count == dimension.getWidth()) {
+                System.out.println("");
+                count = 0;
+            }
+//            System.out.print(".");
+
+//
+//            if (count == dimension.getWidth() / 2) {
+//                System.out.println(".".repeat(3 * dimension.getLength() + 1));
+//            }
         }
-        System.out.print("\\ ");
-        System.out.print(".".repeat((dimension.getLength() - 1) * 4));
-        System.out.println(". /");
     }
 }
