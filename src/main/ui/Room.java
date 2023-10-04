4e027463896977d8231dbaa92450e47fca920392
Dikpaal <dikpaal@outlook.com>
2023-10-04 10:21:08 -0700

    refactored printRoom() and created a createPlane() method

--- src/main/ui/Room.java
+++ src/main/ui/Room.java
@@ -2,39 +2,53 @@ package ui;
 
 import model.Dimension;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Room {
     Dimension dimension = new Dimension(10, 10);
     List<List<String>> plane;
 
+    // REQUIRES: nothing
+    // MODIFIES: nothing
+    // EFFECTS: prints the room to the console
     public void printRoom() {
+
+        List<List<String>> tempList = createPlane();
+
+        for (List<String> lst : tempList) {
+            for (String s : lst) {
+                int number = Integer.parseInt(s);
+                if (number < 10) {
+                    System.out.print("   " + number + " ");
+                } else if (number < 100) {
+                    System.out.print("  " + number + " ");
+                }
+                if (number >= 100) {
+                    System.out.print(" " + number + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    // REQUIRES: nothing
+    // MODIFIES: this
+    // EFFECTS: creates a plane for a given dimension
+
+    public List createPlane() {
 
         int count = 0;
 
-        for (int i = 0; i < dimension.getLength() * dimension.getWidth(); i++) {
-            int number = i + 1;
-
-            if (number < 10) {
-                System.out.print("   " + number + " ");
-            } else if (number < 100) {
-                System.out.print("  " + number + " ");
+        List<List<String>> tempList = new ArrayList<>();
+        for (int i = 1; i <= dimension.getWidth(); i++) {
+            List<String> subTempList = new ArrayList<>();
+            for (int j = 1; j <= dimension.getLength(); j++) {
+                subTempList.add("" + (count + j) + "");
             }
-            if (number >= 100) {
-                System.out.print(" " + number + " ");
-            }
-            count++;
-
-            if (count == dimension.getWidth()) {
-                System.out.println("");
-                count = 0;
-            }
-//            System.out.print(".");
-
-//
-//            if (count == dimension.getWidth() / 2) {
-//                System.out.println(".".repeat(3 * dimension.getLength() + 1));
-//            }
+            tempList.add(subTempList);
+            count += dimension.getLength();
         }
+        return tempList;
     }
 }
