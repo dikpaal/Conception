456bad4eab2101a9bac209231c69b6dc97e974c9
Dikpaal <dikpaal@outlook.com>
2023-10-05 14:25:52 -0700

    added introductory user interaction

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         Room r = new Room();
-        r.printRoomWithDashes();
+        r.mainUserInput();
 
     }
 }
--- src/main/ui/Room.java
+++ src/main/ui/Room.java
@@ -4,16 +4,53 @@ import model.Dimension;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Room {
-    Dimension dimension = new Dimension(10, 10);
+    String username;
+    Dimension dimension;
     List<List<String>> plane;
+
+
+    // REQUIRES: nothing
+    // MODIFIES: nothing
+    // EFFECTS: simulates a user interface which accepts input from the user
+    public void mainUserInput() {
+        introductionOfUser();
+        userChoosesDimension();
+        printRoomWithDashes();
+    }
+
+
+    // REQUIRES: nothing
+    // MODIFIES: nothing
+    // EFFECTS: inputs and stores the username from the user
+    public void introductionOfUser() {
+        Scanner s = new Scanner(System.in);
+        System.out.println();
+        System.out.println("What do we call you?");
+        String username = s.nextLine();
+        System.out.println();
+        setUsername(username);
+        System.out.println("Hello, " + getUsername());
+    }
+
+    // REQUIRES: nothing
+    // MODIFIES: nothing
+    // EFFECTS: inputs and stores the dimension from the user
+    public void userChoosesDimension() {
+        Scanner s1 = new Scanner(System.in);
+        System.out.println();
+        System.out.println("What would the dimension of your room be? ");
+        String dimensionString = s1.nextLine();
+        int dimensionInt = Integer.parseInt(dimensionString);
+        setDimension(dimensionInt);
+    }
 
     // REQUIRES: nothing
     // MODIFIES: nothing
     // EFFECTS: prints the room to the console
     public void printRoomWithNumbers() {
-
         List<List<String>> tempList = createPlane();
 
         for (List<String> lst : tempList) {
@@ -36,7 +73,6 @@ public class Room {
     // MODIFIES: nothing
     // EFFECTS: prints a room with numbers substituted with dashes
     public void printRoomWithDashes() {
-
         List<List<String>> tempList = createPlane();
 
         for (List<String> lst : tempList) {
@@ -52,7 +88,6 @@ public class Room {
     // MODIFIES: this
     // EFFECTS: creates a plane for a given dimension
     public List createPlane() {
-
         int count = 0;
 
         List<List<String>> tempList = new ArrayList<>();
@@ -65,5 +100,37 @@ public class Room {
             count += dimension.getLength();
         }
         return tempList;
+    }
+
+    // GETTERS
+
+    // REQUIRES: nothing
+    // MODIFIES: nothing
+    // EFFECTS: returns the username of the user who created this room
+    public String getUsername() {
+        return this.username;
+    }
+
+    // REQUIRES: nothing
+    // MODIFIES: nothing
+    // EFFECTS: returns the dimension of the room
+    public Dimension getDimension() {
+        return this.dimension;
+    }
+
+    // SETTERS
+
+    // REQUIRES: s != ""
+    // MODIFIES: this
+    // EFFECTS: sets the username of the user who created this room to s
+    public void setUsername(String s) {
+        this.username = s;
+    }
+
+    // REQUIRES: d > 0
+    // MODIFIES: this
+    // EFFECTS: sets the dimension of the room to new DImension(d, d)
+    public void setDimension(int d) {
+        this.dimension = new Dimension(d, d);
     }
 }
