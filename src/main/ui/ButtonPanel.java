cf050b93e53126cc44e0a7142e111f6dc124fb2e
Dikpaal <dikpaal@outlook.com>
2023-11-14 10:34:43 -0800

    added the chair sofa and centre table buttons

--- src/main/ui/ButtonPanel.java
+++ src/main/ui/ButtonPanel.java
@@ -2,39 +2,47 @@ package ui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
 
 public class ButtonPanel extends PanelGUI {
 
     public ButtonPanel(int w, int h) {
-        JLabel chairLabel = new JLabel();
         ImageIcon chairIcon = new ImageIcon("src/main/ui/images/chair.png");
         Image chairImg = chairIcon.getImage();
         Image newChairImg = chairImg.getScaledInstance(30, 30,  java.awt.Image.SCALE_SMOOTH);
         ImageIcon newChairIcon = new ImageIcon(newChairImg);
-        chairLabel.setIcon(newChairIcon);
 
-        JLabel sofaLabel = new JLabel();
         ImageIcon sofaIcon = new ImageIcon("src/main/ui/images/sofa.png");
         Image sofaImg = sofaIcon.getImage();
         Image newSofaImg = sofaImg.getScaledInstance(60, 30,  java.awt.Image.SCALE_SMOOTH);
         ImageIcon newSofaIcon = new ImageIcon(newSofaImg);
-        sofaLabel.setIcon(newSofaIcon);
 
-        JLabel centreTableLabel = new JLabel();
         ImageIcon centreTableIcon = new ImageIcon("src/main/ui/images/centretable.png");
         Image centreTableImg = centreTableIcon.getImage();
         Image newCentreTableImg = centreTableImg.getScaledInstance(30, 30,  java.awt.Image.SCALE_SMOOTH);
         ImageIcon newCentreTableIcon = new ImageIcon(newCentreTableImg);
-        centreTableLabel.setIcon(newCentreTableIcon);
 
-        chairLabel.setBounds(w - 230, h / 6, 30, 30);
-        sofaLabel.setBounds(w - 180, h / 6, 60, 30);
-        centreTableLabel.setBounds(w - 100, h / 6, 30, 30);
+        JButton chairButton = new JButton();
+        JButton sofaButton = new JButton();
+        JButton centreTableButton = new JButton();
+
+        chairButton.setBounds(w - 230, h / 6, 30, 30);
+        sofaButton.setBounds(w - 180, h / 6, 60, 30);
+        centreTableButton.setBounds(w - 100, h / 6, 30, 30);
+
+        chairButton.setIcon(newChairIcon);
+        chairButton.addActionListener(e -> System.out.println("Chair button pressed!"));
+
+        sofaButton.setIcon(newSofaIcon);
+        sofaButton.addActionListener(e -> System.out.println("Sofa button pressed!"));
+
+        centreTableButton.setIcon(newCentreTableIcon);
+        centreTableButton.addActionListener(e -> System.out.println("Centre Table button pressed!"));
 
         this.setLayout(null);
-        this.add(chairLabel);
-        this.add(sofaLabel);
-        this.add(centreTableLabel);
+        this.add(chairButton);
+        this.add(sofaButton);
+        this.add(centreTableButton);
 
         this.setBackground(Color.white);
         this.setBounds(0, 0, w, h);
