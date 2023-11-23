8acc4d0bb2ab945c5b6db91af1c05bb9769d9324
Dikpaal <dikpaal@outlook.com>
2023-11-23 11:51:30 -0800

    this is just a test commit

--- src/test/persistence/JSONReaderTest.java
+++ src/test/persistence/JSONReaderTest.java
@@ -1,41 +1,41 @@
-//package persistence;
-//
-//import model.Chair;
-//import model.Furniture;
-//import model.Room;
-//import model.Sofa;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import ui.ConsoleUI;
-//
-//import java.io.FileNotFoundException;
-//import java.io.IOException;
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//// Represents a class that tests the JsonReader
-//class JsonReaderTest {
-//
-//    @Test
-//    void testReaderNonExistentFile() {
-//        Room r = new Room(3);
-//        r.setUsername("Dikpaal");
-//        ConsoleUI consoleUI = new ConsoleUI();
-//        consoleUI.setRoom(r);
-//        r.setNumberedPlane(consoleUI.createNumberedPlane());
-//        consoleUI.initiateNumberedAndFurnitureList();
-//
-//        JsonWriter writer = new JsonWriter("./data/noSuchFile.json");
-//        try {
-//            writer.open();
-//            writer.write(r);
-//            writer.close();
-//        } catch (FileNotFoundException e) {
-//            //
-//        }
-//
+package persistence;
+
+import model.Chair;
+import model.Furniture;
+import model.Room;
+import model.Sofa;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ui.ConsoleUI;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+// Represents a class that tests the JsonReader
+class JsonReaderTest {
+
+    @Test
+    void testReaderNonExistentFile() {
+        Room r = new Room(3);
+        r.setUsername("Dikpaal");
+        ConsoleUI consoleUI = new ConsoleUI();
+        consoleUI.setRoom(r);
+        r.setNumberedPlane(consoleUI.createNumberedPlane());
+        consoleUI.initiateNumberedAndFurnitureList();
+
+        JsonWriter writer = new JsonWriter("./data/noSuchFile.json");
+        try {
+            writer.open();
+            writer.write(r);
+            writer.close();
+        } catch (FileNotFoundException e) {
+            //
+        }
+
 //        JsonReader reader = new JsonReader("./data/noSuchFile.json");
 //        try {
 //            r = reader.read();
@@ -43,82 +43,82 @@
 //        } catch (IOException e) {
 //            // pass
 //        }
-//    }
-//
-//    @Test
-//    void testReaderEmptyRoom() {
-//        Room r = new Room(3);
-//        r.setUsername("Dikpaal");
-//        ConsoleUI consoleUI = new ConsoleUI();
-//        consoleUI.setRoom(r);
-//        r.setNumberedPlane(consoleUI.createNumberedPlane());
-//        consoleUI.initiateNumberedAndFurnitureList();
-//
-//        JsonWriter writer = new JsonWriter("./data/testReaderEmptyRoom.json");
-//        try {
-//            writer.open();
-//            writer.write(r);
-//            writer.close();
-//        } catch (FileNotFoundException e) {
-//            //
-//        }
-//
-//        JsonReader reader = new JsonReader("./data/testReaderEmptyRoom.json");
-//        try {
-//            r = reader.read();
-//            assertEquals(3, r.getDimension().getLength());
-//            assertEquals(0, r.getFurnitureList().size());
-//        } catch (IOException e) {
-//            fail("Couldn't read from file");
-//        }
-//    }
-//
-//    @Test
-//    void testReaderGeneralWorkRoom() {
-//        Room r = new Room(3);
-//        r.setUsername("Dikpaal");
-//        ConsoleUI consoleUI = new ConsoleUI();
-//        consoleUI.setRoom(r);
-//        r.setNumberedPlane(consoleUI.createNumberedPlane());
-//        consoleUI.initiateNumberedAndFurnitureList();
-//
-//        Furniture chair = new Chair();
-//        chair.setSpot(1);
-//
-//        Furniture sofa = new Sofa();
-//        sofa.setSpotsForSofa(3, 4);
-//
-//        r.addToFurnitureList(chair);
-//        r.addToFurnitureList(sofa);
-//
-//        JsonWriter writer = new JsonWriter("./data/testReaderGeneralRoom.json");
-//        try {
-//            writer.open();
-//            writer.write(r);
-//            writer.close();
-//        } catch (FileNotFoundException e) {
-//            //
-//        }
-//
-//        JsonReader reader = new JsonReader("./data/testReaderGeneralRoom.json");
-//        try {
-//            r = reader.read();
-//            assertEquals(3, r.getDimension().getLength());
-//            List<Furniture> furnitureList = r.getFurnitureList();
-//            assertEquals(2, furnitureList.size());
-//
-//
-//            assertEquals(1, r.getFurnitureList().get(0).getSpot());
-//
-//            List<Integer> spots = new ArrayList<>();
-//            spots.add(3);
-//            spots.add(4);
-//
-//            assertEquals(spots, r.getFurnitureList().get(1).getAllSpots());
-//
-//
-//        } catch (IOException e) {
-//            fail("Couldn't read from file");
-//        }
-//    }
-//}
+    }
+
+    @Test
+    void testReaderEmptyRoom() {
+        Room r = new Room(3);
+        r.setUsername("Dikpaal");
+        ConsoleUI consoleUI = new ConsoleUI();
+        consoleUI.setRoom(r);
+        r.setNumberedPlane(consoleUI.createNumberedPlane());
+        consoleUI.initiateNumberedAndFurnitureList();
+
+        JsonWriter writer = new JsonWriter("./data/testReaderEmptyRoom.json");
+        try {
+            writer.open();
+            writer.write(r);
+            writer.close();
+        } catch (FileNotFoundException e) {
+            //
+        }
+
+        JsonReader reader = new JsonReader("./data/testReaderEmptyRoom.json");
+        try {
+            r = reader.read();
+            assertEquals(3, r.getDimension().getLength());
+            assertEquals(0, r.getFurnitureList().size());
+        } catch (IOException e) {
+            fail("Couldn't read from file");
+        }
+    }
+
+    @Test
+    void testReaderGeneralWorkRoom() {
+        Room r = new Room(3);
+        r.setUsername("Dikpaal");
+        ConsoleUI consoleUI = new ConsoleUI();
+        consoleUI.setRoom(r);
+        r.setNumberedPlane(consoleUI.createNumberedPlane());
+        consoleUI.initiateNumberedAndFurnitureList();
+
+        Furniture chair = new Chair();
+        chair.setSpot(1);
+
+        Furniture sofa = new Sofa();
+        sofa.setSpotsForSofa(3, 4);
+
+        r.addToFurnitureList(chair);
+        r.addToFurnitureList(sofa);
+
+        JsonWriter writer = new JsonWriter("./data/testReaderGeneralRoom.json");
+        try {
+            writer.open();
+            writer.write(r);
+            writer.close();
+        } catch (FileNotFoundException e) {
+            //
+        }
+
+        JsonReader reader = new JsonReader("./data/testReaderGeneralRoom.json");
+        try {
+            r = reader.read();
+            assertEquals(3, r.getDimension().getLength());
+            List<Furniture> furnitureList = r.getFurnitureList();
+            assertEquals(2, furnitureList.size());
+
+
+            assertEquals(1, r.getFurnitureList().get(0).getSpot());
+
+            List<Integer> spots = new ArrayList<>();
+            spots.add(3);
+            spots.add(4);
+
+            assertEquals(spots, r.getFurnitureList().get(1).getAllSpots());
+
+
+        } catch (IOException e) {
+            fail("Couldn't read from file");
+        }
+    }
+}
--- src/test/persistence/JSONWriterTest.java
+++ src/test/persistence/JSONWriterTest.java
@@ -1,82 +1,82 @@
-//package persistence;
-//
-//import com.sun.tools.jconsole.JConsoleContext;
-//import model.Chair;
-//import model.Furniture;
-//import model.Room;
-//import model.Sofa;
-//import org.junit.jupiter.api.Test;
-//import ui.ConsoleUI;
-//
-//import java.io.IOException;
-//import java.util.List;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//// Represents a class that tests the JsonWriter
-//class JsonWriterTest {
-//    //NOTE TO CPSC 210 STUDENTS: the strategy in designing tests for the JsonWriter is to
-//    //write data to a file and then use the reader to read it back in and check that we
-//    //read in a copy of what was written out.
-//
-//    @Test
-//    void testWriterInvalidFile() {
-//        try {
-//            Room r = new Room(3);
-//            JsonWriter writer = new JsonWriter("./data/my\0illegal:fileName.json");
-//            writer.open();
-//            fail("IOException was expected");
-//        } catch (IOException e) {
-//            // pass
-//        }
-//    }
-//
-//    @Test
-//    void testWriterEmptyWorkroom() {
-//        try {
-//            Room r = new Room(3);
-//            r.setUsername("Dikpaal");
-//            ConsoleUI consoleUI = new ConsoleUI();
-//            consoleUI.setRoom(r);
-//            r.setNumberedPlane(consoleUI.createNumberedPlane());
-//            consoleUI.initiateNumberedAndFurnitureList();
-//            JsonWriter writer = new JsonWriter("./data/testWriterEmptyWorkroom.json");
-//            writer.open();
-//            writer.write(r);
-//            writer.close();
-//
-//            JsonReader reader = new JsonReader("./data/testWriterEmptyWorkroom.json");
-//            r = reader.read();
-//            assertEquals(3, r.getDimension().getLength());
-//            assertEquals(0, r.getFurnitureList().size());
-//        } catch (IOException e) {
-//            fail("Exception should not have been thrown");
-//        }
-//    }
-//
-//    @Test
-//    void testWriterGeneralWorkroom() {
-//        try {
-//            Room r = new Room(3);
-//            r.setUsername("Dikpaal");
-//            ConsoleUI consoleUI = new ConsoleUI();
-//            consoleUI.setRoom(r);
-//            r.setNumberedPlane(consoleUI.createNumberedPlane());
-//            consoleUI.initiateNumberedAndFurnitureList();
-//            r.addToFurnitureList(new Chair());
-//            r.addToFurnitureList(new Sofa());
-//            JsonWriter writer = new JsonWriter("./data/testWriterGeneralWorkroom.json");
-//            writer.open();
-//            writer.write(r);
-//            writer.close();
-//
-//            JsonReader reader = new JsonReader("./data/testWriterGeneralWorkroom.json");
-//            r = reader.read();
-//            assertEquals(3, r.getDimension().getLength());
-//            List<Furniture> thingies = r.getFurnitureList();
-//            assertEquals(2, thingies.size());
-//        } catch (IOException e) {
-//            fail("Exception should not have been thrown");
-//        }
-//    }
-//}
+package persistence;
+
+import com.sun.tools.jconsole.JConsoleContext;
+import model.Chair;
+import model.Furniture;
+import model.Room;
+import model.Sofa;
+import org.junit.jupiter.api.Test;
+import ui.ConsoleUI;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+// Represents a class that tests the JsonWriter
+class JsonWriterTest {
+    //NOTE TO CPSC 210 STUDENTS: the strategy in designing tests for the JsonWriter is to
+    //write data to a file and then use the reader to read it back in and check that we
+    //read in a copy of what was written out.
+
+    @Test
+    void testWriterInvalidFile() {
+        try {
+            Room r = new Room(3);
+            JsonWriter writer = new JsonWriter("./data/my\0illegal:fileName.json");
+            writer.open();
+            fail("IOException was expected");
+        } catch (IOException e) {
+            // pass
+        }
+    }
+
+    @Test
+    void testWriterEmptyWorkroom() {
+        try {
+            Room r = new Room(3);
+            r.setUsername("Dikpaal");
+            ConsoleUI consoleUI = new ConsoleUI();
+            consoleUI.setRoom(r);
+            r.setNumberedPlane(consoleUI.createNumberedPlane());
+            consoleUI.initiateNumberedAndFurnitureList();
+            JsonWriter writer = new JsonWriter("./data/testWriterEmptyWorkroom.json");
+            writer.open();
+            writer.write(r);
+            writer.close();
+
+            JsonReader reader = new JsonReader("./data/testWriterEmptyWorkroom.json");
+            r = reader.read();
+            assertEquals(3, r.getDimension().getLength());
+            assertEquals(0, r.getFurnitureList().size());
+        } catch (IOException e) {
+            fail("Exception should not have been thrown");
+        }
+    }
+
+    @Test
+    void testWriterGeneralWorkroom() {
+        try {
+            Room r = new Room(3);
+            r.setUsername("Dikpaal");
+            ConsoleUI consoleUI = new ConsoleUI();
+            consoleUI.setRoom(r);
+            r.setNumberedPlane(consoleUI.createNumberedPlane());
+            consoleUI.initiateNumberedAndFurnitureList();
+            r.addToFurnitureList(new Chair());
+            r.addToFurnitureList(new Sofa());
+            JsonWriter writer = new JsonWriter("./data/testWriterGeneralWorkroom.json");
+            writer.open();
+            writer.write(r);
+            writer.close();
+
+            JsonReader reader = new JsonReader("./data/testWriterGeneralWorkroom.json");
+            r = reader.read();
+            assertEquals(3, r.getDimension().getLength());
+            List<Furniture> thingies = r.getFurnitureList();
+            assertEquals(2, thingies.size());
+        } catch (IOException e) {
+            fail("Exception should not have been thrown");
+        }
+    }
+}
